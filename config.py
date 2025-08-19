4b8d21c2a4e6a2c0afbbb3090e7a243a67557728
waheeb mahyoob <134747118+waheeb71@users.noreply.github.com>
2025-08-19 21:19:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,10 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 SERVICE_ACCOUNT_FILE = os.getenv("SERVICE_ACCOUNT_FILE")
 FIREBASE_DB_URL = os.getenv("FIREBASE_DB_URL")
 ADMIN_NOTIFICATION_ID = int(os.getenv("ADMIN_NOTIFICATION_ID"))
-
+GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
+GEMINI_VISION_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro-vision:generateContent"
 
 # Bot signature
 BOT_SIGNATURE = "\n\n━━━━━━━━━━━━━━\n📢 قناة التلجرام: @SyberSc71\n👨‍💻 برمجة:  @WAT4F"
+
 
