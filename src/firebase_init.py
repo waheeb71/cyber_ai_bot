96357b3d9d76f083da388040bbff7032fa4a9afe
waheeb mahyoob <134747118+waheeb71@users.noreply.github.com>
2025-08-20 16:48:54 +0300

    Update firebase_init.py
    new

--- src/firebase_init.py
+++ src/firebase_init.py
@@ -1,18 +1,23 @@
 import os
 import json
+import firebase_admin
+from firebase_admin import credentials, db
 
-=
+
 service_account_content = os.getenv("SERVICE_ACCOUNT_JSON")
+firebase_db_url = os.getenv("FIREBASE_DB_URL")
 
-# أنشئ ملف serviceAccountKey.json مؤقت
+if not service_account_content or not firebase_db_url:
+    raise ValueError("SERVICE_ACCOUNT_JSON or FIREBASE_DB_URL is missing!")
+
+
 with open("serviceAccountKey.json", "w") as f:
     f.write(service_account_content)
 
 
-import firebase_admin
-from firebase_admin import credentials, db
-
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
-    'databaseURL': os.getenv("FIREBASE_DB_URL")
+    'databaseURL': firebase_db_url
 })
+
+print("Firebase initialized successfully ✅")
